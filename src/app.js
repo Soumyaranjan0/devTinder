592ab644291671474f0bbdfa9f47a36cce4ad56a
soumyaranjan0 <soumyaranjanbehera012@gmail.com>
2024-11-09 13:43:45 +0530

    Elporing route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
 
-app.use('/test', function (req, res) {
-  res.send('Hello from the server')
+app.get("/user",(req,res)=>{
+    res.send({"firstname":"soumya","lastname":"behera"})
 })
+
+app.post("/user",(req,res)=>{
+    res.send("Data successfully saved to the Database")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Data successfully deleted to the Database")
+})
+
+
+//this will match all the HTTP method API calls to Test
 app.use('/hello', function (req, res) {
-  res.send('Hello hello hello')
+    res.send('Hello hello hello')
 })
+
+
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
